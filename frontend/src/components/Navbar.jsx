42ea2795d63b3e93fdcd6b1d2e5f6c64574ab932
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-09 18:54:04 +0530

    added icons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
+  const [visible,setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} className='w-56' alt='logo' />
@@ -30,10 +31,28 @@ function Navbar() {
         <img src={assets.search_icon} className='w-5.5 cursor-pointer' alt="search_icon" />
         <div className='group relative'>
           <img src={assets.profile_icon} className='w-5.5 cursor-pointer' alt="profile_icon" />
-
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>My Profile</p>
+              <p className='cursor-pointer hover:text-black'>Orders</p>
+              <p className='cursor-pointer hover:text-black'>Logout</p>
+            </div>
+          </div>
         </div>
+        <Link to='/cart' className='relative'>
+          <img src={assets.cart_icon} className='w-5.5 min-w-5.5' alt='cart_icon' />
+          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
+            10
+          </p>
+        </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5.5 cursor-pointer sm:hidden' alt="menu_icon" />
+      </div>
+      {/* Sidebar menue for small screens*/}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ?'w-full':'w-0'}`}>
+
       </div>
     </div>
+
   )
 }
 
