9e3eba907d13f787a6c02cf236445bf142a2ae55
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-10 10:50:31 +0530

    title component

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 
 const LatestCollection = () => {
+    const {products} = useContext(ShopContext);
   return (
-    <div>
-      
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+        <p></p>
+      </div>
     </div>
   )
 }
 
-export default LatestCollection
+export default LatestCollection;
