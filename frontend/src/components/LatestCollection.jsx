db23279015c95730dc33e32b6e53684aa52cf06b
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-10 11:40:44 +0530

    latest Collection page

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,17 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
-    const {products} = useContext(ShopContext);
-  return (
-    <div className='my-10'>
-      <div className='text-center py-8 text-3xl'>
-        <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
-        <p></p>
-      </div>
-    </div>
-  )
+    const { products } = useContext(ShopContext);
+    const [lastProducts, setLastProducts] = useState([])
+
+    useEffect(() => {
+        setLastProducts(products.slice(0, 10));
+    }, [])
+    return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+                <p className='w-3/4 m-auto text-xs sm:text-base text-gray-600'>
+                    Discover our freshest picks, hand-selected to bring style, comfort, and quality right to your doorstep. Explore what’s trending this season and find your next favorite piece.
+                </p>
+            </div>
+            {/* Rendering Products */}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {
+                    lastProducts.map((item,index) => (
+                        <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default LatestCollection;
