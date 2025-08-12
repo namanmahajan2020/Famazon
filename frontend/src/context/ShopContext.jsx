21df360829c41a3aba52519c2b3f30adda96d80d
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-12 10:18:37 +0530

    selecting size and adding toast for error message

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,15 +1,43 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
-    const [search,setSearch]=useState('')
-    const[showSearch,setShowSearch]=useState(false)
+    const [search, setSearch] = useState('')
+    const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Select Product Size')
+            return;
+        }
+
+
+        if (cardData[itemId]) {
+            if (cardData[itemId][size]) {
+                cardData[itemId][size] += 1;
+            }
+            else {
+                cardData[itemId][size] = 1;
+            }
+        }
+        else {
+            cardData[itemId] = {};
+            cardData[itemId][size] = 1;
+        }
+        setCartItems(cardData);
+    }
+
+    useEffect(() => {
+
+    }, [cartItems])
 
     const value = {
-        products,currency,delivery_fee,search,setSearch,showSearch,setShowSearch
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart
     }
     return (
         <ShopContext.Provider value={value}>
