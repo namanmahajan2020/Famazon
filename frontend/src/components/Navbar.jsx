7e7528aea2c1a37f5ec90982f55bdc0e199f0c03
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-14 18:46:18 +0530

    login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,9 +31,11 @@ function Navbar() {
 
       </ul>
       <div className='flex items-center gap-6'>
-        <img onClick={()=>setShowSearch(!showSearch)} src={assets.search_icon} className='w-5.5 cursor-pointer' alt="search_icon" />
+        <img onClick={()=>setShowSearch(!showSearch)} src={assets.search_icon} className='w-5 cursor-pointer' alt="search_icon" />
         <div className='group relative'>
-          <img src={assets.profile_icon} className='w-5.5 cursor-pointer' alt="profile_icon" />
+          <Link to='/login'>
+          <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="profile_icon" />
+          </Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -43,7 +45,7 @@ function Navbar() {
           </div>
         </div>
         <Link to='/cart' className='relative'>
-          <img src={assets.cart_icon} className='w-5.5 min-w-5.5' alt='cart_icon' />
+          <img src={assets.cart_icon} className='w-5 min-w-5' alt='cart_icon' />
           <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
             {getCartCount()}
           </p>
