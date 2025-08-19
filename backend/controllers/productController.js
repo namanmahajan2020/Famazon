deb968bd3420a15d5a8fec8f3847906bab6b00ef
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-19 10:29:30 +0530

    singleProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -71,7 +71,7 @@ const removeProduct = async (req, res) => {
     try {
 
         await productModel.findByIdAndDelete(req.body.id)
-        res.json({ success: true, messsage:"Product Removed" })
+        res.json({ success: true, messsage: "Product Removed" })
 
     } catch (error) {
         console.log(error)
@@ -83,6 +83,17 @@ const removeProduct = async (req, res) => {
 
 // function for single product info
 const singleProduct = async (req, res) => {
+
+    try {
+
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({ success: true, product })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
