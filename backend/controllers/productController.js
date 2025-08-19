7267e979966dd9ffa377791defe47467776a4efc
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-19 10:11:24 +0530

    listProducts

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,6 +52,16 @@ const addProduct = async (req, res) => {
 // function for list product 
 const listProducts = async (req, res) => {
 
+    try {
+
+        const products = await productModel.find({});
+        res.json({ success: true, products })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
 }
 
 
