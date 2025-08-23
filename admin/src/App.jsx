8fb4fbf18155583e91f190306c189f08ec19ab42
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-23 12:13:40 +0530

    Add product from admin panel frontend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -24,15 +24,15 @@ const App = () => {
       {
         token === "" ? <Login setToken={setToken} /> :
           <>
-            <Navbar />
+            <Navbar setToken={setToken} />
             <hr className='border-gray-200' />
             <div className='flex w-full'>
               <Sidebar />
               <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
                 <Routes>
-                  <Route path='/add' element={<Add />}></Route>
-                  <Route path='/list' element={<List />}></Route>
-                  <Route path='/orders' element={<Orders />}></Route>
+                  <Route path='/add' element={<Add token={token} />}></Route>
+                  <Route path='/list' element={<List token={token} />}></Route>
+                  <Route path='/orders' element={<Orders token={token} />}></Route>
                 </Routes>
               </div>
             </div>
