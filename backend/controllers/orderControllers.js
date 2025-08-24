9ba451db2dd2201ace6a7090d7bf3df853731acf
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-25 00:18:00 +0530

    order Router

---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,38 @@
+
+
+//placing order using cod method
+const placeOrder = async (req, res) => {
+
+}
+
+
+//placing order using Stripe method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+
+//placing order using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+
+//All Orders data for admin pannel
+const allOrders = async (req, res) => {
+
+}
+
+
+//User Orders data for Frontend
+const userOrders = async (req, res) => {
+
+}
+
+
+//update Orders status from admin panel
+const updateStatus = async (req, res) => {
+
+}
+
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
