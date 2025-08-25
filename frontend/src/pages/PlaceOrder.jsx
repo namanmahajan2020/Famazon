d4ec2c6150ac668c71e5099fc6b39a15e39564cb
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-25 13:47:34 +0530

    loading Order Data in frontend

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -3,10 +3,12 @@ import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
 import { ShopContext } from '../context/ShopContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const PlaceOrder = () => {
   const [method, setMethod] = useState('cod');
-  const { navigate, backendUrl, token, cartItems, setCartItem, getCartAmount, delivery_fee, products } = useContext(ShopContext);
+  const { navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, delivery_fee, products } = useContext(ShopContext);
 
   const [formData, setFormData] = useState({
     firstName: '',
@@ -46,10 +48,37 @@ const PlaceOrder = () => {
           }
         }
       }
-console.log(orderItems)
+
+      let orderData = {
+        address: formData,
+        items: orderItems,
+        amount: getCartAmount() + delivery_fee
+      }
+
+      switch (method) {
+
+        //Api calls for COD
+        case 'cod':
+          const response = await axios.post(backendUrl + '/api/order/place', orderData, { headers: { token } })
+
+          if (response.data.success) {
+            setCartItems({})
+            navigate('/orders')
+          }
+          else {
+            toast.error(response.data.message)
+          }
+          break;
+
+        default:
+          break;
+
+      }
+
     }
     catch (error) {
-
+      console.log(error)
+      toast.error(error.message)
     }
 
   }
