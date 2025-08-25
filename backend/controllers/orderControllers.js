5c13540f068efcc3db84d48d2cfc7679b0412b5c
Naman Mahajan <mahajannaman2020@gmail.com>
2025-08-25 23:00:25 +0530

    order status changing

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -86,6 +86,18 @@ const userOrders = async (req, res) => {
 //update Orders status from admin panel
 const updateStatus = async (req, res) => {
 
+    try {
+
+        const { orderId, status } = req.body
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({ success: true, message: 'status Updated' })
+
+    }
+    catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
 }
 
 export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus }
